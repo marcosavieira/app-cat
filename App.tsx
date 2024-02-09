9d740a59b15cc364a20e35efe040da3394394b3b
devmarcosav@gmail.com <devmarcosav@gmail.com>
2024-02-09 18:01:10 -0300

    feat: create react-native app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  Image, 
+  TouchableOpacity,
+} from 'react-native';
+import React, { useState } from 'react';
+import catColor from './assets/catYeys.png';
+import catBlack from './assets/catBlack.jpg';
 
 export default function App() {
+  const [isActive, setIsActive] = useState(false);
+  let imgName = isActive ? catColor : catBlack;
+
+
+  function handleState(){
+    setIsActive((oldValue:boolean) => {
+      return !oldValue
+    });
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.textStyle}>Toque no gato</Text>
+      <TouchableOpacity onPress={handleState}>
+
+      <Image  source={imgName}></Image>
+      </TouchableOpacity>
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle: {
+    color: 'red',
+    fontSize: 30,
+  }
+
 });
